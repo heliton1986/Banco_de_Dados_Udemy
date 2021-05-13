550a74c7dc4e4c71b39f5af71e5d94b2287910b6
Heliton Ribeiro Ferreira <heliton.ds86@gmail.com>
2021-05-13 16:51:52 -0300

    Add 003.select_where_operadores_comparacao.sql

---
+++ 002.Inserindo_consultando_ atualizando/003.select_where_operadores_comparacao.sql
@@ -0,0 +1,21 @@
+SELECT *
+FROM aluno
+WHERE sexo = 'f';
+
+SELECT *
+from aluno
+where Valor_Pago_Curso < 600;
+
+SELECT *
+from aluno
+where Valor_Pago_Curso > 600;
+
+SELECT *
+from aluno
+where Valor_Pago_Curso <= 600;
+
+SELECT * FROM  aluno;
+
+SELECT *
+from aluno
+WHERE Valor_Pago_Curso >= 655.45;
