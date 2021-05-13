bc60af3d66208059a1b2bda69e80a2b54c98ac9c
Heliton Ribeiro Ferreira <heliton.ds86@gmail.com>
2021-05-13 17:28:05 -0300

    Add 005.select_filtros_operador_between.sql

---
+++ 002.Inserindo_consultando_ atualizando/005.select_filtros_operador_between.sql
@@ -0,0 +1,8 @@
+SELECT * FROM aluno;
+
+SELECT nome, idade FROM aluno
+WHERE Idade BETWEEN 40 AND 60;
+
+SELECT nome, data_inscricao_curso FROM aluno
+WHERE Data_Inscricao_Curso BETWEEN '2018-11-01' AND '2018-11-30';
+
