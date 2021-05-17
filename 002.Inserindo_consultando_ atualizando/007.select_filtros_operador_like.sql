1ac137554ec2d677ab192fc647ba6cf76c4eeecb
Heliton Ribeiro Ferreira <heliton.ds86@gmail.com>
2021-05-17 19:15:13 -0300

    Add query 007.select_filtros_operador_like.sql

---
+++ 002.Inserindo_consultando_ atualizando/007.select_filtros_operador_like.sql
@@ -0,0 +1,39 @@
+USE Universidade_U;
+
+SELECT *
+from aluno;
+
+SELECT * FROM aluno
+WHERE nome LIKE 'Maria';
+
+SELECT * FROM aluno
+WHERE nome LIKE '%o';
+
+SELECT * FROM aluno
+WHERE nome LIKE '%r%';
+
+SELECT * FROM aluno
+WHERE nome LIKE 'm%';
+
+SELECT * FROM aluno
+WHERE nome LIKE 'm%a';
+
+SELECT * FROM aluno
+WHERE nome LIKE '_osé';
+
+SELECT * FROM aluno
+WHERE nome LIKE '_oã_';
+
+SELECT * FROM aluno
+WHERE nome LIKE '___é';
+
+SELECT * FROM aluno
+WHERE nome LIKE '%d_';
+
+
+
+
+
+
+
+
